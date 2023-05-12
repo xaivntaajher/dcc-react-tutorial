a21a5993bd661d54212a1fb876530ac8e305b7b2
Xaivntaaj Her <dn_her@yahoo.com>
2023-05-12 13:43:24 -0500

    complete tutorial and updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
+import './App.css';
 
 function App() {
   // can only update a state variable (entries) through it's function (setEntries)
@@ -16,10 +17,24 @@ function App() {
 
   return (
     // addNewEntry with no () is used as reference, or a pointer and can execute the function, whereas with () is referencing the result.
-    <div>
-      <DisplayEntries parentEntries={entries}/>
-      <AddEntryForm addNewEntryProperty={addNewEntry}/> 
-      <EntriesChartTracker parentEntries={entries}/>
+    <div className='container-fluid'>
+      <div className='row'>
+        <h3 style={{margin: '1em'}}>Weight 
+        <small className='text-muted'>Tracker</small></h3>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <DisplayEntries parentEntries={entries}/>
+          </div>
+          <div className='border-box'>
+            <AddEntryForm addNewEntryProperty={addNewEntry}/> 
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <EntriesChartTracker parentEntries={entries}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
